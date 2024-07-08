40e2a52f69c90b6d4ca61e4be72d4f44924e4058
oJa6aN <mrg0zp@gmail.com>
2024-07-09 01:00:03 +0900

    a

---
+++ Create_Janction.ps1
@@ -0,0 +1,7 @@
+﻿$source = "C:\Users\rei\AppData\Roaming\SBI Securities"
+$target = "E:\Users\rei\AppData\Roaming\SBI Securities"
+
+if ((Test-Path $source) -and -not (Test-Path $target)) {
+    Move-Item -Path $source -Destination $target
+    New-Item -ItemType Junction -Path $source -Target $target
+}
