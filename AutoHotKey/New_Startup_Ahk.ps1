4c63e88d72f7cfcb6ddbdc01e3ee1fbe657258bc
oJa6aN <mrg0zp@gmail.com>
2024-11-13 17:00:05 +0900

    *

---
+++ AutoHotKey/New_Startup_Ahk.ps1
@@ -0,0 +1,29 @@
+﻿# shell:startup
+
+function New-StartupAhk {
+    param (
+        [string]$targetPath,
+        [string]$shortcutPath
+    )
+
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $shortcut = $WScriptShell.CreateShortcut($shortcutPath)
+    $shortcut.TargetPath = $targetPath
+    $shortcut.Save()
+}
+
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
+$startupFolder = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("Startup"))
+
+Get-ChildItem -Path $startupFolder -Filter "*.ahk.lnk" | ForEach-Object {
+    Remove-Item -Path $_.FullName -Force
+    Write-Output "Deleted existing shortcut: $($_.Name)"
+}
+
+Get-ChildItem -Path $scriptDir -Filter "*.ahk" | ForEach-Object {
+    $ahkFile = $_.FullName
+    $shortcutPath = [System.IO.Path]::Combine($startupFolder, "$($_.BaseName).ahk.lnk")
+
+    New-StartupAhk -targetPath $ahkFile -shortcutPath $shortcutPath
+    Write-Output "Created shortcut for $($_.Name) in Startup folder"
+}
---
+++ AutoHotKey/README.md
@@ -0,0 +1,31 @@
+# Customize my PC with AutoHotKey
+
+## Install Autohotkey
+
+[AutoHotkey](https://www.autohotkey.com/)
+
+## Configure VSCode for AutoHotkey
+
+### Install the Extension
+
+Install the AutoHotkey extension, [AHK++ (AutoHotkey Plus Plus)](https://marketplace.visualstudio.com/items?itemName=mark-wiemer.vscode-autohotkey-plus-plus), in VSCode. This can be done from the Visual Studio Marketplace.
+
+### Costomize settings.json
+
+Adjust the `helpPath` and `interpreterPath` based on your system.
+
+```json:./vscode/settings.json
+{
+  "AHK++.v2.file": {
+    "compileBaseFile": "",
+    "helpPath": "C:/Program Files/AutoHotkey/v2/AutoHotkey.chm",
+    "interpreterPath": "C:/Program Files/AutoHotkey/v2/AutoHotkey.exe",
+    "maxScanDepth": 2,
+    "templateSnippetName": "AhkTemplateV2"
+  }
+}
+```
+
+## Automatically Build AHK on Startup
+
+Use `New_Shortcuts.ps1` PowerShell script to automate AHK-related tasks on startup.
