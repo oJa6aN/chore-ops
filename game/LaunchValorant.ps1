5146f22d3712b5459f64679f4a2a142e9bbed839
oJa6aN <mrg0zp@gmail.com>
2025-04-07 14:35:42 +0900

    *

--- game/LaunchValorant.ps1
+++ game/LaunchValorant.ps1
@@ -1,11 +1,9 @@
-﻿$riotClient = "C:\Riot Games\Riot Client\RiotClientServices.exe"
-$riotArgs = "--launch-product=valorant --launch-patchline=live"
+﻿Start-Process -FilePath "C:\Riot Games\Riot Client\RiotClientServices.exe" -ArgumentList "--launch-product=valorant --launch-patchline=live"
 
-Start-Process -FilePath $riotClient -ArgumentList $riotArgs
 Write-Output "Launching VALORANT via Riot Client..."
 
 $process = $null
-for ($i = 0; $i -lt 180; $i++) {
+for ($i = 0; $i -lt 300; $i++) {
   Start-Sleep -Seconds 1
   $process = Get-Process -Name "VALORANT-Win64-Shipping" -ErrorAction SilentlyContinue
   if ($process) {
@@ -17,12 +15,11 @@ for ($i = 0; $i -lt 180; $i++) {
 if ($process) {
   try {
     $process.PriorityClass = "High"
-    Write-Output "Priority set to High."
   }
   catch {
     Write-Error "Failed to set priority: $_"
   }
 }
 else {
-  Write-Warning "VALORANT process not found within 180 seconds."
+  Write-Warning "VALORANT process not found within 300 seconds."
 }
